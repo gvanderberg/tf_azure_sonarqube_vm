2a67e4ca5ab617a1dd81af2e2417f4d0c1ee548c
Gareth van der Berg <gareth.shaun@gmail.com>
2021-03-24 09:22:31 +0200

    update

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -10,11 +10,10 @@ cd /downloads
 
 wget $SONARQUBE_ZIP_URL
 unzip sonarqube-enterprise-${SONARQUBE_VERSION}.zip
+rm -rf sonarqube-enterprise-${SONARQUBE_VERSION}.zip
 
 cp -rf /opt/sonarqube/conf/* /downloads/sonarqube-${SONARQUBE_VERSION}/conf/
 
-mkdir /opt/sonarqube-${SONARQUBE_VERSION}
-
-mv sonarqube-${SONARQUBE_VERSION}/* /opt/sonarqube-${SONARQUBE_VERSION}
+mv sonarqube-${SONARQUBE_VERSION} /opt
 
 chown -R sonarqube:sonarqube /opt/sonarqube-${SONARQUBE_VERSION}
