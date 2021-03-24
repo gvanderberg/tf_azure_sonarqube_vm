1d51e890380a56fd68ddc1711339774261ad4664
Gareth van der Berg <gareth.shaun@gmail.com>
2021-03-24 10:26:03 +0200

    update install version

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,7 @@ apt-get install -y \
   unzip \
   wget
 
-SONARQUBE_VERSION="8.7.0.41497"
+SONARQUBE_VERSION="8.7.1.42226"
 SONARQUBE_ZIP_URL="https://binaries.sonarsource.com/CommercialDistribution/sonarqube-enterprise/sonarqube-enterprise-${SONARQUBE_VERSION}.zip"
 
 mkdir -p /downloads
