24f368f9601bd5481ea1e1d0b5dda35ba4b9542e
Gareth van der Berg <gareth.shaun@gmail.com>
2021-10-01 16:14:00 +0200

    update upgrade script

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,6 +2,7 @@
 
 set -eux
 
+CURRENT_DATE=$(date "+%Y%m%d")
 SONARQUBE_VERSION="9.1.0.47736"
 SONARQUBE_ZIP_URL="https://binaries.sonarsource.com/CommercialDistribution/sonarqube-enterprise/sonarqube-enterprise-${SONARQUBE_VERSION}.zip"
 
@@ -18,3 +19,15 @@ cp -rf /opt/sonarqube/extensions/plugins/* /downloads/sonarqube-${SONARQUBE_VERS
 mv sonarqube-${SONARQUBE_VERSION} /opt
 
 chown -R sonarqube:sonarqube /opt/sonarqube-${SONARQUBE_VERSION}
+
+systemctl stop sonarqube.service
+
+mv /opt/sonarqube /opt/sonarqube-${CURRENT_DATE}
+mv /opt/sonarqube-${SONARQUBE_VERSION} /opt/sonarqube
+
+sed -i.bak "s/sonar-application-.*/sonar-application-${SONARQUBE_VERSION}.jar/g" /etc/systemd/system/sonarqube.service
+
+systemctl daemon-reload
+systemctl start sonarqube.service
+
+echo "To perform database migration after upgrade open url: https://sonarqube.outsurance.co.za/setup"
